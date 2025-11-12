d16e3783066a4455bf7ffb36815214cc7112ed56
Steve Benedick <sbenedic@adobe.com>
2025-11-12 14:08:14 -0700

    -update to new app property on stage, using concierge config

--- AEPConcierge/Sources/Concierge.swift
+++ AEPConcierge/Sources/Concierge.swift
@@ -110,23 +110,27 @@ public class Concierge: NSObject, Extension {
             return
         }
         
-        // TODO: use server from config
-        let server = "edge-int.adobedc.net"
-//        guard let server = configSharedState.conciergeServer else {
-//            errorMessage = "Unable to show Brand Concierge UI - server information is unavailable from configuration."
-//            return
-//        }
-                
-        // TODO: use datastream from config
-        let datastream = "6acf9d12-5018-4f84-8224-aac4900782f0"
-//        guard let datastream = configSharedState.conciergeDatastream else {
-//            errorMessage = "Unable to show Brand Concierge UI - datastream information is unavailable from configuration."
-//            return
+        // TODO: remove static values used for testing to override config
+//        {
+//            let server = "edge-int.adobedc.net"
+//            let datastream = "6acf9d12-5018-4f84-8224-aac4900782f0"
+//            let surfaces = ["web://edge-int.adobedc.net/brand-concierge/pages/745F37C35E4B776E0A49421B@AdobeOrg/acom_m15/index.html"]
 //        }
         
-        // TODO: use surfaces provided in configuration
-        let surfaces = ["web://edge-int.adobedc.net/brand-concierge/pages/745F37C35E4B776E0A49421B@AdobeOrg/acom_m15/index.html"]
-                
+        guard let server = configSharedState.conciergeServer else {
+            errorMessage = "Unable to show Brand Concierge UI - server information is unavailable from configuration."
+            return
+        }
+        
+        guard let datastream = configSharedState.conciergeDatastream else {
+            errorMessage = "Unable to show Brand Concierge UI - datastream information is unavailable from configuration."
+            return
+        }
+        
+        guard let surfaces = configSharedState.conciergeSurfaces else {
+            errorMessage = "Unable to show Brand Concierge UI - no surfaces were provided in configuration."
+            return
+        }
         
         let config = ConciergeConfiguration(server: server, datastream: datastream, ecid: ecid, surfaces: surfaces)
         let responseEvent = event.createResponseEvent(name: Constants.EventName.SHOW_UI_RESPONSE,
