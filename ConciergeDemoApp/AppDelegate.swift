758c9ec6b079b0c127569af3c077b702cb99b157
Ryan Morales <rymorale@adobe.com>
2026-03-31 11:13:48 -0700

    restore correct app id config

--- ConciergeDemoApp/AppDelegate.swift
+++ ConciergeDemoApp/AppDelegate.swift
@@ -28,13 +28,13 @@ final class AppDelegate: NSObject, UIApplicationDelegate {
         ]
 
         MobileCore.registerExtensions(extensions) {
-            MobileCore.configureWith(appId: "staging/1b50a869c4a2/2993416661b1/launch-03ab810b3d2d-development")
+            MobileCore.configureWith(appId: "staging/1b50a869c4a2/570831bce333/launch-bcc070a55cca-development")
 
             // TODO: - temporary override of datastream and server until we get that sorted out
             MobileCore.updateConfigurationWith(configDict: [
-                            "concierge.server": "edge.adobedc.net",
-                            "concierge.configId": "4eb99a22-309c-471c-a1ab-f82560d87a0d"
-                        ])
+                "concierge.configId": "6acf9d12-5018-4f84-8224-aac4900782f0",
+                "concierge.server": "edge-int.adobedc.net"
+            ])
         }
 
         return true
